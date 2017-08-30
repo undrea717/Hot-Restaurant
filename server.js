c1adb9678b4140d00a15e5e220497afa26a36d79
Almira1612 <almira1612@gmail.com>
2017-08-30 09:50:45 -0400

    added

--- server.js
+++ server.js
@@ -48,8 +48,8 @@ app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.get("/table", function(req, res) {
-  res.sendFile(path.join(__dirname, "table.html"));
+app.get("/tables", function(req, res) {
+  res.sendFile(path.join(__dirname, "tables.html"));
 });
 
 app.get("/reserve", function(req, res) {
